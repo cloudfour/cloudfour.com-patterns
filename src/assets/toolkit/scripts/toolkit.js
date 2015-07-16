3fc3736b59a8427b8972f6692a2d102cace568a4
Tyler Sticka <tyler@tylersticka.com>
2015-07-16 11:23:05 -0700

    Add WIP JavaScript for FloatLabel pattern
    We don't have any other JS needs yet, and it felt larger than this task to define a module structure, so for now I've simply added it to toolkit.js (retaining the `inprogress` label for tha pattern).

--- src/assets/toolkit/scripts/toolkit.js
+++ src/assets/toolkit/scripts/toolkit.js
@@ -3,3 +3,101 @@
  */
 
 'use strict';
+
+/**
+ * TODO: Define and implement module structure for project.
+ */
+
+/**
+ * FloatLabel
+ */
+(function() {
+
+  /**
+   * Utilities
+   */
+
+  var addClass = function(el, className) {
+    if (el.classList) {
+      el.classList.add(className);
+    } else {
+      el.className += ' ' + className;
+    }
+  };
+
+  var removeClass = function(el, className) {
+    if (el.classList) {
+      el.classList.remove(className);
+    } else {
+      el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+    }
+  };
+
+  var extend = function(out) {
+    out = out || {};
+
+    for (var i = 1; i < arguments.length; i++) {
+      if (!arguments[i]) {
+        continue;
+      }
+
+      for (var key in arguments[i]) {
+        if (arguments[i].hasOwnProperty(key)) {
+          out[key] = arguments[i][key];
+        }
+      }
+    }
+
+    return out;
+  };
+
+  /**
+   * Module
+   */
+
+  var FloatLabel = function(element, options) {
+    this.element = element;
+    this.options = extend({}, FloatLabel.DEFAULTS, options);
+    if (typeof this.options.input === 'string') {
+      this.input = this.element.querySelectorAll(this.options.input)[0];
+    } else {
+      this.input = this.options.input;
+    }
+    this.init();
+  };
+
+  FloatLabel.DEFAULTS = {
+    input: 'input',
+    className: 'is-empty',
+    eventName: 'keyup'
+  };
+
+  FloatLabel.prototype.init = function() {
+    this.element.addEventListener(this.options.eventName, function() {
+      this.run();
+    }.bind(this));
+    this.run();
+  };
+
+  FloatLabel.prototype.isEmpty = function() {
+    return !this.input.value.trim().length;
+  };
+
+  FloatLabel.prototype.run = function() {
+    if (this.isEmpty()) {
+      addClass(this.element, this.options.className);
+    } else {
+      removeClass(this.element, this.options.className);
+    }
+  };
+
+  /**
+   * Use module
+   */
+
+  var elements = document.querySelectorAll('.js-FloatLabel');
+  Array.prototype.forEach.call(elements, function (element) {
+    new FloatLabel(element);
+  });
+
+}());

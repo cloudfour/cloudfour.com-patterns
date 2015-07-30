3f7fdaeea12572413090c8a385c4e88ff9d6e350
Tyler Sticka <tyler@tylersticka.com>
2015-07-30 13:38:05 -0700

    Add hasClass and toggleClass modules
    These seem really helpful, and in browsers that don't support el.classList the hasClass method will be required for toggleClass.
    
    Also, template strings!! 😱

--- src/assets/toolkit/scripts/lib/dom/attributes.js
+++ src/assets/toolkit/scripts/lib/dom/attributes.js
@@ -17,3 +17,25 @@ export function removeClass (el, className) {
     el.className = classNameItems.slice(classNameIndex).join(' ');
   }
 }
+
+export function hasClass (el, className) {
+  if (el.classList) {
+    return el.classList.contains(className);
+  }
+  return new RegExp(`(^| )${className}( |$)`, 'gi').test(el.className);
+}
+
+export function toggleClass (el, className, force) {
+  if (el.classList) {
+    return el.classList.toggle(className, force);
+  }
+  if (typeof force === 'undefined') {
+    force = ! hasClass(el, className);
+  }
+  if (force) {
+    addClass(el, className);
+  } else {
+    removeClass(el, className);
+  }
+  return force;
+}

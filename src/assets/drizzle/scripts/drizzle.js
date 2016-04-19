24ce7c2f9b4d85e815bc293f4544151c6d828621
Erik Jung <erik@cloudfour.com>
2016-04-18 19:25:32 -0700

    Swap out JS

---
+++ src/assets/drizzle/scripts/drizzle.js
@@ -0,0 +1,43 @@
+'use strict';
+
+import 'prismjs';
+
+class DrizzleDom {
+  constructor () {
+    this.navToggle.addEventListener('click', event => {
+      event.preventDefault();
+      this.toggleNav();
+    });
+    this.setActiveNavItem(window.location.pathname);
+  }
+
+  get nav () {
+    return document.getElementById('nav');
+  }
+
+  get navMenu () {
+    return document.getElementById('nav-menu');
+  }
+
+  get navToggle () {
+    return this.nav.querySelector('a[href="#nav"]');
+  }
+
+  get navLinks () {
+    return this.navMenu.querySelectorAll('a');
+  }
+
+  toggleNav () {
+    this.nav.classList.toggle('is-active');
+  }
+
+  setActiveNavItem (pathname) {
+    const isMatch = a => a.pathname === pathname;
+    const item = Array.from(this.navLinks).find(isMatch);
+    if (item) {
+      item.classList.add('is-active');
+    }
+  }
+}
+
+const drizzleDom = new DrizzleDom();

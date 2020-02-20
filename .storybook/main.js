7258789132b69c5e9d2525f6f8ce17bffadf758d
Tyler Sticka <tyler@tylersticka.com>
2020-02-20 15:40:28 -0800

    Add Twig support

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,11 @@
 module.exports = {
   stories: ['../src/**/*.stories.js'],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.twig$/,
+      use: 'twigjs-loader'
+    });
+
+    return config;
+  }
 };
--- src/demo/button/button.stories.js
+++ src/demo/button/button.stories.js
@@ -1,11 +1,7 @@
+const template = require('./button.twig');
+
 export default {
   title: 'Demo',
 };
 
-export const Button = () => {
-  const btn = document.createElement('button');
-  btn.type = 'button';
-  btn.innerText = 'Hello Button';
-  btn.addEventListener('click', e => console.log(e));
-  return btn;
-};
+export const Button = () => template();

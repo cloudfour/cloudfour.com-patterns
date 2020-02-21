ad246c1addbcdd1b6f3bb51e5b61a71cb16524d7
Tyler Sticka <tyler@tylersticka.com>
2020-02-20 16:19:56 -0800

    Add Sass loader

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,15 @@ module.exports = {
   stories: ['../src/**/*.stories.(js|mdx)'],
   addons: [
     '@storybook/addon-a11y/register',
-    '@storybook/addon-docs'
+    '@storybook/addon-docs',
+    {
+      name: '@storybook/preset-scss',
+      options: {
+        sassLoaderOptions: {
+          implementation: require('sass')
+        }
+      }
+    }
   ],
   webpackFinal: async (config) => {
     config.module.rules.push({

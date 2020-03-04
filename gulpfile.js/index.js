e8a882d71b646ed91d9e1a313481544d830a5d45
Tyler Sticka <tyler@tylersticka.com>
2020-03-04 14:26:16 -0800

    Add citation to comment

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -5,6 +5,8 @@
  *
  * This means running `npx gulp hello-world` will run the task exported by
  * `tasks/hello-world.js` if it exists.
+ *
+ * @see https://gulpjs.com/docs/en/getting-started/javascript-and-gulpfiles#splitting-a-gulpfile
  */
 
 module.exports = require('require-dir')('./tasks');

eaad0cb5c8ec916aaa63ce71838e20610ce0e44a
Tyler Sticka <tyler@tylersticka.com>
2020-03-04 14:25:41 -0800

    Add comment to base gulpfile

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,1 +1,10 @@
+/**
+ * Every JavaScript file in the `tasks/` directory will be exported with its
+ * basename used as the key. These will then be read by Gulp as individual task
+ * definitions.
+ *
+ * This means running `npx gulp hello-world` will run the task exported by
+ * `tasks/hello-world.js` if it exists.
+ */
+
 module.exports = require('require-dir')('./tasks');

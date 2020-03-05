fa68ad82909e933a2184b447c6c076fb24389af3
Tyler Sticka <tyler@cloudfour.com>
2020-03-05 10:39:41 -0800

    Basic Button pattern (#519)
    * Button
    
    * Add focus-visible
    
    * Add button style modifiers
    
    * Remove demos

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,7 @@ import { addDecorator, addParameters } from '@storybook/html';
 import { withA11y } from '@storybook/addon-a11y';
 import centered from '@storybook/addon-centered/html';
 import * as colors from '../src/design-tokens/colors.yml';
+import 'focus-visible';
 import './preview.scss';
 
 // Add decorators to all stories

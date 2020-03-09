5c7316905fd95ff1e3f8a2419de57f740bcb2aad
Scott Vandehey <scott@cloudfour.com>
2020-03-09 11:24:04 -0700

    add welcome page, loaded first

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 const { resolve } = require('path');
 
 module.exports = {
-  stories: ['../src/**/*.stories.(js|mdx)'],
+  stories: ['../src/welcome.stories.mdx', '../src/**/*.stories.(js|mdx)'],
   addons: [
     // Core addons
     '@storybook/addon-a11y/register',

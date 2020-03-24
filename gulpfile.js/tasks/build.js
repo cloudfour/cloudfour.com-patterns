f0c1db622dd83149ab399086bb4ff5bd8d85b005
Caleb Eby <caleb.eby01@gmail.com>
2020-03-24 14:26:52 -0700

    Add js build + entry fields to package.json

--- gulpfile.js/tasks/build.js
+++ gulpfile.js/tasks/build.js
@@ -1,14 +1,21 @@
 const { src, dest, parallel } = require('gulp');
+
 const sass = require('gulp-sass');
 const rename = require('gulp-rename');
 const postcss = require('gulp-postcss');
 const cssnano = require('cssnano');
 
+const rollup = require('rollup');
+const path = require('path');
+const multiEntry = require('@rollup/plugin-multi-entry');
+const nodeResolve = require('@rollup/plugin-node-resolve');
+const { terser } = require('rollup-plugin-terser');
+
 sass.compiler = require('sass');
 
 const outDir = 'dist';
 
-function buildSass() {
+const buildSass = () => {
   return src('./src/index.scss')
     .pipe(
       sass({
@@ -21,13 +28,31 @@ function buildSass() {
     .pipe(postcss([cssnano()]))
     .pipe(rename({ extname: '.min.css' }))
     .pipe(dest(outDir));
-}
+};
 
-function buildJS() {}
+const buildJS = async () => {
+  const name = 'cloudfourPatterns';
+  const bundle = await rollup.rollup({
+    input: 'src/{objects,components}/**/*.js',
+    plugins: [multiEntry(), nodeResolve()]
+  });
+  await Promise.all([
+    bundle.write({ format: 'esm', file: path.join(outDir, `${name}.mjs`) }),
+    bundle.write({
+      format: 'umd',
+      name,
+      file: path.join(outDir, `${name}.js`)
+    }),
+    bundle.write({
+      format: 'umd',
+      name,
+      file: path.join(outDir, `${name}.min.js`),
+      plugins: [terser()]
+    })
+  ]);
+};
 
-function build() {
-  return buildSass();
-}
+const build = parallel(buildSass, buildJS);
 
 // Expose to Gulp
 module.exports = build;

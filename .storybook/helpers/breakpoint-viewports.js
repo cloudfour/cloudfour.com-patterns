087855817c02d53d195b88e59b35658af5f0bf3b
Josh Schneider <josh@cloudfour.com>
2020-04-06 15:47:52 -0700

    Revert "Add typedef to breakpointViewports" - Additional typing not needed
    This reverts commit 4947b1eea37a3ca82b07b8f4a0beed2a185f4818.

--- .storybook/helpers/breakpoint-viewports.js
+++ .storybook/helpers/breakpoint-viewports.js
@@ -2,7 +2,6 @@ import * as breakpoints from '../../src/design-tokens/breakpoint.yml';
 
 /**
  * List of viewports to use with addon-viewport, based on Theo breakpoints tokens
- * @typedef {import('@storybook/addon-viewport/ts3.5/dist/models').ViewportMap}
  */
 export const breakpointViewports = Object.keys(breakpoints).map((name) => {
   return {

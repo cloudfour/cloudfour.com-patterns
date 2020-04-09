4ba7919e87f0ee0ce56ff9ee1a91a921a13b9261
Josh Schneider <josh@cloudfour.com>
2020-04-09 16:21:24 -0700

    Updates TS definitions, moves typedef and elaborates in comments

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -10,16 +10,18 @@ import 'focus-visible';
 import '../src/index.scss';
 import './preview.scss';
 
-/**
- * @typedef { [String, import('@storybook/client-api/dist/types').StoreItem] } StoryItem
- */
-
 // Accessibility testing via aXe
 addDecorator(withA11y);
 
 // Theme selection from stories
 const themes = [{ name: 'Dark', class: 't-dark', color: colors.primaryBrand }];
 addParameters({ themes });
+
+/**
+ * Define the type of value passed to `addParameters.options.storySort`, which is
+ * parsed to get to the name of each story's category
+ * @typedef { [String, import('@storybook/client-api/dist/types').StoreItem] } StoryItem
+ */
 
 /**
  * Get the category from a `storySort` story
@@ -47,17 +49,22 @@ const orderedCategories = [
 /**
  * Sanitize (slufigy) each entry in the list of categories, to normalize odd characters,
  * capitalization, and use of spaces vs dashes. Do this outside of the `storySort`
- * sorting function
+ * sorting function. Each `StoryItem`'s category will also be passed to `sanitize`
+ * for comparison with values in this list.
  */
 const sanitizedCategories = orderedCategories.map((kind) => sanitize(kind));
 
 /**
  * Compares two stories and sorts by category, according to a predefined order
  * @param {String[]} sanitizedCategories - Sanitized array of categories to use for sorting
- * @returns {(StoryItem, StoryItem) => (0 | 1 | -1)} - Sorts two stories based on
+ * @returns {(a: StoryItem, b: StoryItem) => (0 | 1 | -1)} - Sorts two stories based on
  * the order of the passed-in array of categories
  */
 const storySort = (sanitizedCategories) => (a, b) => {
+  /**
+   * Each `StoryItem`'s category is passed to `sanitize` for comparison with values
+   * in this list.
+   */
   const indexA = sanitizedCategories.indexOf(sanitize(getStoryCategory(a)));
   const indexB = sanitizedCategories.indexOf(sanitize(getStoryCategory(b)));
   return indexA === indexB ? 0 : indexA > indexB ? 1 : -1;

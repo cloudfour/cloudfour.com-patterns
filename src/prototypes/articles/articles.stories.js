19477146ca2c69e705f17293f9e000d37521f695
Tyler Sticka <tyler@tylersticka.com>
2020-04-10 16:55:13 -0700

    Add working (?) starting point

--- src/prototypes/articles/articles.stories.js
+++ src/prototypes/articles/articles.stories.js
@@ -1,6 +1,8 @@
 import feed from './data/feed.json';
 import examplePrototype from './example/example.twig';
+import dsPrototype from './ds/example-01.twig';
 import './example/example.scss';
+import './ds/example-01.scss';
 
 export default {
   title: 'Prototypes/Articles',
@@ -11,3 +13,8 @@ export default {
 };
 
 export const Example = () => examplePrototype({ feed });
+
+export const Derek = () => dsPrototype({ feed });
+Derek.story = {
+  name: 'Derek’s Prototype', // Only necessary if you want to customize name
+};

5087044ed0aaa3a558925776962e6d07a77ae00f
Danielle Romo <danielle@cloudfour.com>
2020-04-14 12:07:41 -0700

    Add cards

--- src/prototypes/articles/articles.stories.js
+++ src/prototypes/articles/articles.stories.js
@@ -1,6 +1,8 @@
 import feed from './data/feed.json';
 import examplePrototype from './example/example.twig';
+import daniellePrototype from './danielle/articles-dr.twig';
 import './example/example.scss';
+import './danielle/articles-dr.scss';
 
 export default {
   title: 'Prototypes/Articles',
@@ -11,3 +13,5 @@ export default {
 };
 
 export const Example = () => examplePrototype({ feed });
+
+export const Danielle = () => daniellePrototype({ feed });

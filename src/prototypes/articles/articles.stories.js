c6aa2ce4254628046a19c6f715b914fe99f30931
Tyler Sticka <tyler@tylersticka.com>
2020-04-17 17:05:59 -0700

    Add new prototype

--- src/prototypes/articles/articles.stories.js
+++ src/prototypes/articles/articles.stories.js
@@ -3,8 +3,10 @@ import feed from './data/feed.json';
 import topics from './data/topics.json';
 import examplePrototype from './example/example.twig';
 import tylerPrototypeA from './tyler/a.twig';
+import tylerPrototypeB from './tyler/b.twig';
 import './example/example.scss';
-import './tyler/tyler.scss';
+import './tyler/a.scss';
+import './tyler/b.scss';
 
 export default {
   title: 'Prototypes/Articles',
@@ -17,3 +19,4 @@ export default {
 export const Example = () => examplePrototype({ essentials, feed, topics });
 
 export const TylerA = () => tylerPrototypeA({ essentials, feed, topics });
+export const TylerB = () => tylerPrototypeB({ essentials, feed, topics });

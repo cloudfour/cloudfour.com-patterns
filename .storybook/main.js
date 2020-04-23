3897a8912ec8bddb741cf07f4d20283bcb162bf4
Josh Schneider <josh@cloudfour.com>
2020-04-23 14:54:18 -0700

    Chore: Add CSS sourcemaps (#643)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,5 @@
 const { resolve } = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   // We load the welcome story separately so it will be the first sidebar item.
@@ -14,6 +15,7 @@ module.exports = {
     'storybook-addon-paddings',
   ],
   webpackFinal: async (config) => {
+    const isDev = config.mode === 'development';
     // Remove default SVG processing from default config.
     // @see https://github.com/storybookjs/storybook/issues/5708#issuecomment-515384927
     config.module.rules = config.module.rules.map((data) => {
@@ -23,27 +25,49 @@ module.exports = {
       return data;
     });
 
+    /**
+     * For development, leave the default 'cheap-module-source-map', as it's faster and works.
+     * For the build, using the default does not work correctly, but this option appears to.
+     * @see https://webpack.js.org/configuration/devtool/
+     */
+    if (!isDev) {
+      config.devtool = 'source-map';
+    }
+
     // Push new rules
     config.module.rules.push(
       {
         test: /\.s[ca]ss$/,
         use: [
-          'style-loader',
+          {
+            // @see https://github.com/webpack-contrib/style-loader/issues/303#issuecomment-581168870
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: isDev,
+            },
+          },
           {
             loader: 'css-loader',
             options: {
+              sourceMap: true,
               // Lets CSS loader know there are two loaders left that may be
               // handling imports.
               // @see https://github.com/webpack-contrib/css-loader#importloaders
               importLoaders: 2,
             },
           },
-          'postcss-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
           {
             loader: 'sass-loader',
             options: {
               // Dart Sass is easier to install than Node Sass
               implementation: require('sass'),
+              sourceMap: true,
               sassOptions: {
                 importer: [
                   require('../glob-sass-importer'),
@@ -70,6 +94,8 @@ module.exports = {
         use: '@svgr/webpack',
       }
     );
+
+    config.plugins.push(new MiniCssExtractPlugin());
 
     return config;
   },

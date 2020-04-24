e1035c76e5a2d8b2b534cf41eac72f7ba7392435
Josh Schneider <josh@cloudfour.com>
2020-04-24 15:46:36 -0700

    CSS polyfills (#652)

--- postcss.config.js
+++ postcss.config.js
@@ -3,5 +3,13 @@ module.exports = {
     require('postcss-inline-svg')({
       paths: ['./src/assets'],
     }),
+    /**
+     * Enables Stage 2 options, by default.
+     * This includes logical properties and values.
+     * @see https://preset-env.cssdb.org/features#logical-properties-and-values
+     * @see https://github.com/csstools/postcss-preset-env#stage
+     * @see https://github.com/csstools/postcss-preset-env/blob/master/INSTALL.md#postcss-cli
+     */
+    require('postcss-preset-env')(),
   ],
 };

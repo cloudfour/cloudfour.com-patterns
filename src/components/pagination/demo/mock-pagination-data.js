8c14effdfbc685c3573200acac84ce9eb205f854
Tyler Sticka <tyler@tylersticka.com>
2020-05-07 16:18:22 -0700

    Add pagination component

---
+++ src/components/pagination/demo/mock-pagination-data.js
@@ -0,0 +1,50 @@
+function mockPageData(page = 1, current = 1) {
+  const data = {
+    title: page,
+    text: page,
+    name: page,
+  };
+
+  if (page === current) {
+    data.current = true;
+  } else {
+    data.link = `#page-${page}`;
+  }
+
+  return data;
+}
+
+export default function mockPaginationData({
+  current = 1,
+  midSize = 2,
+  total = 36,
+} = {}) {
+  const data = { current, total, mid_size: midSize, pages: [] };
+  let start = current - midSize;
+  let end = current + midSize;
+
+  if (start < 1) {
+    end += 1 - start;
+  }
+
+  if (end > total) {
+    start -= end - total;
+  }
+
+  start = Math.max(1, start);
+  end = Math.min(total, end);
+
+  for (let i = start; i <= end; i++) {
+    data.pages.push(mockPageData(i, current));
+  }
+
+  if (current > 1) {
+    data.prev = mockPageData(current - 1, current);
+  }
+
+  if (current < total) {
+    data.next = mockPageData(current + 1, current);
+  }
+
+  return data;
+}

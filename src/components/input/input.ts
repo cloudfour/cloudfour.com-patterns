db53f2bafcc18286c3fa026e05833040b6420f59
Scott Vandehey <scott@cloudfour.com>
2020-05-18 16:57:03 -0700

    add outline of how to proceed

--- src/components/input/input.ts
+++ src/components/input/input.ts
@@ -1,14 +1,14 @@
-export const elasticTextarea = (a: string) => {
-  console.log('elastic textarea initialized', a);
+// Here's the dumb little add eventlister fucniton
+export const createElasticTextarea = (element: Element) => {
+  // TODO: add event listener running update command inline
+  console.log('elastic textarea initialized', element);
+
+  // Return an object with the destroy method
+  // so users can remove the event listener if needed
+  return {
+    destroy() {
+      // TODO: remove event listener
+      console.log('DESTROY', element);
+    },
+  };
 };
-
-// Console.log('LOADED!', document.body.innerHTML);
-
-// const elasticTextareas = document.querySelectorAll('.js-elastic-textarea');
-
-// console.log('ELEMENTS', elasticTextareas.length);
-
-// elasticTextareas.forEach(() => {
-//   console.log('FOR EACH');
-//   elasticTextarea('YO!');
-// });

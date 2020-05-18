1f64df3b90740bd57237fc219e5a2df9d1800a24
Scott Vandehey <scott@cloudfour.com>
2020-05-18 15:32:43 -0700

    load JS into story after render

--- src/components/input/input.ts
+++ src/components/input/input.ts
@@ -2,13 +2,13 @@ export const elasticTextarea = (a: string) => {
   console.log('elastic textarea initialized', a);
 };
 
-console.log('LOADED!');
+// Console.log('LOADED!', document.body.innerHTML);
 
-const elasticTextareas = document.querySelectorAll('.js-elastic-textarea');
+// const elasticTextareas = document.querySelectorAll('.js-elastic-textarea');
 
-console.log('ELEMENTS', elasticTextareas.length);
+// console.log('ELEMENTS', elasticTextareas.length);
 
-elasticTextareas.forEach(() => {
-  console.log('FOR EACH');
-  elasticTextarea('YO!');
-});
+// elasticTextareas.forEach(() => {
+//   console.log('FOR EACH');
+//   elasticTextarea('YO!');
+// });

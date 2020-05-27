e4efb095f2dede6388ad5c6542e04d899352b159
Scott Vandehey <scott@cloudfour.com>
2020-05-27 16:31:14 -0700

    use aliases in cypress test

--- src/components/sky-nav/sky-nav.cypress.ts
+++ src/components/sky-nav/sky-nav.cypress.ts
@@ -4,30 +4,30 @@ import { createSkyNav } from './sky-nav';
 import menu from './demo/menu.json';
 
 describe('Sky Nav', () => {
-  it('On small screens the menu starts closed & the menu button opens it', () => {
-    render(skyNavMarkup({ includeMainDemo: true, menu }));
-    const navButton = cy.get('.js-sky-nav-menu-toggle');
-    navButton.then((navButton) => {
-      createSkyNav(navButton.get(0) as HTMLButtonElement);
-    });
+  it('On small screens the menu starts closed & the button opens it', () => {
+    render(
+      skyNavMarkup({
+        includeMainDemo: true,
+        menu,
+      })
+    );
     cy.viewport('iphone-6');
-
-    // Note: The following set of `findByRole` selectors feels redundant, and
-    // we'd like to save them to variables, but that causes the test to fail
-    // for reasons that are unclear. For now, we'll just keep finding. - SV
+    cy.findByRole('button')
+      .as('button')
+      .then((navButton) => {
+        createSkyNav(navButton.get(0) as HTMLButtonElement);
+      });
 
     // Initial state: menu is closed
-    cy.findByRole('button').should('have.attr', 'aria-expanded', 'false');
+    cy.get('@button').should('have.attr', 'aria-expanded', 'false');
     cy.findByRole('list').should('not.exist');
 
-    cy.findByRole('button').click();
-
     // After click: menu is open
-    cy.findByRole('button').should('have.attr', 'aria-expanded', 'true');
+    cy.get('@button').click().should('have.attr', 'aria-expanded', 'true');
     cy.findByRole('list').should('not.have.attr', 'hidden');
   });
 
-  it('On large screens the menu button is hidden', () => {
+  it('On large screens the menu is visible & the button is hidden', () => {
     render(skyNavMarkup({ includeMainDemo: true, menu }));
     const navButton = cy.get('.js-sky-nav-menu-toggle');
     navButton.then((navButton) => {

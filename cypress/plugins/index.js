950f98dfd9bfdee730210c0a90d65add517bce73
Caleb Eby <caleb.eby01@gmail.com>
2020-05-28 13:50:57 -0700

    Change cypress to use webpack

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,16 +1,35 @@
 // @ts-nocheck
 
-// Cypress uses browserify by default to bundle user-code into the test runner
-// By requiring this preprocessor directly, we can add support for importing twig files
-const browserify = require('@cypress/browserify-preprocessor');
+const webpackPreprocessor = require('@cypress/webpack-preprocessor');
+const {
+  twingEnvironmentPlugin,
+  twingOptions,
+} = require('../../twing-environment-webpack-plugin');
 
-const browserifyOptions = browserify.defaultOptions;
-browserifyOptions.typescript = require.resolve('typescript');
-browserifyOptions.browserifyOptions.transform.push([
-  require.resolve('./twig-transform'),
-  {},
-]);
+const getWebpackOptions = async () => ({
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.twig$/,
+        use: {
+          loader: 'twing-loader',
+          options: twingOptions,
+        },
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.mjs', '.js', '.ts', '.tsx'],
+  },
+  plugins: [await twingEnvironmentPlugin()],
+});
 
-module.exports = (on, config) => {
-  on('file:preprocessor', browserify(browserifyOptions));
+module.exports = async (on, config) => {
+  const webpackOptions = await getWebpackOptions();
+  on('file:preprocessor', webpackPreprocessor({ webpackOptions }));
 };

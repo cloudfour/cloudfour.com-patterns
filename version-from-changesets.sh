f296ed0b64d0a3e5be4a8406292cda45523f65c4
Caleb Eby <caleb.eby01@gmail.com>
2020-06-02 14:44:04 -0700

    test

---
+++ version-from-changesets.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Exit if anything fails
+set -e
+
+# Update package.json with new version, combine all the changesets files into the changelog
+changesets version
+
+NEW_VERSION=$(cat package.json | jq -r ".version")
+
+# changesets version does not update package-lock.json or create a commit, or create a tag. npm version does all of those
+# npm version requires clean working tree, so changes are stashed
+git stash
+npm version "${NEW_VERSION}"
+git stash pop
+

94c695d9b2e7bc6fcda24d99113feefc19f93f60
Tyler Sticka <tyler@tylersticka.com>
2020-06-10 12:04:57 -0700

    Fix for TypeScript issues with tokens

--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,9 @@ declare module '*.twig' {
   function template(opts: Record<string, unknown>): string;
   export default template;
 }
+
+declare module '*.yml' {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const tokens: Record<string, any>;
+  export = tokens;
+}

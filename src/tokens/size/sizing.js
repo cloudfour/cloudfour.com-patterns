9d5e461af24b960ea8d14879bc7445d6d8f2ea97
Scott Vandehey <scott@cloudfour.com>
2021-03-03 14:38:51 -0800

    fix build

--- src/tokens/size/sizing.js
+++ src/tokens/size/sizing.js
@@ -8,12 +8,31 @@ module.exports = {
         multiline: { value: modularEm(6) },
       },
     },
+    width: {
+      card_column: {
+        min: {
+          value: '15em',
+          comment: 'The minimum space to display a default card in a grid.',
+        },
+      },
+    },
     square: {
+      avatar: {
+        medium: { value: modularEm(5) },
+        full: { value: '100%' },
+      },
       control: {
         icon: { value: modularEm(0) },
       },
       toggle: {
         medium: { value: modularEm(2) },
+      },
+    },
+    overlap: {
+      small: {
+        value: modularEm(-6),
+        comment:
+          'Small amount of overlap between adjacent items organized in a visual bunch.',
       },
     },
   },

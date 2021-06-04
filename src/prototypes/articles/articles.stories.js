10461a6ded8dbaca11e9f35bfd437dc05f6c33a7
Tyler Sticka <tyler@cloudfour.com>
2021-06-03 17:15:24 -0700

    paddings.disabled → paddings.disable (#1261)

--- src/prototypes/articles/articles.stories.js
+++ src/prototypes/articles/articles.stories.js
@@ -8,7 +8,7 @@ export default {
   title: 'Prototypes/Articles',
   parameters: {
     docs: { page: null },
-    paddings: { disabled: true },
+    paddings: { disable: true },
   },
 };
 
--- src/prototypes/card-variations/card-variations.stories.js
+++ src/prototypes/card-variations/card-variations.stories.js
@@ -5,7 +5,7 @@ export default {
   title: 'Prototypes/Card Variations',
   parameters: {
     docs: { page: null },
-    paddings: { disabled: true },
+    paddings: { disable: true },
   },
 };
 

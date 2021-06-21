a0f528dd0898670eaaf5e5d0101f28e109b5fdc1
Arianna Chau <ariannapikachau@gmail.com>
2021-06-21 09:28:03 -0700

    Remove logo and replace WM image

--- src/prototypes/case-study-listings/case-study-listings.stories.js
+++ src/prototypes/case-study-listings/case-study-listings.stories.js
@@ -1,10 +1,7 @@
 import examplePrototype from './example/example.twig';
 import cpeImage from './images/collage-cpe.jpg';
-import wmImage from './images/collage-wm.jpg';
+import wmImage from './images/walmart.jpg';
 import cfImage from './images/collage-cf.jpg';
-import cpeLogo from './images/cpe-logo.jpg';
-import wmLogo from './images/wm-logo.svg';
-import cfLogo from './images/cf-logo.jpg';
 import './example/example.scss';
 
 export default {
@@ -20,7 +17,4 @@ export const Example = () =>
     cpe: cpeImage,
     wm: wmImage,
     cf: cfImage,
-    cpeL: cpeLogo,
-    wmL: wmLogo,
-    cfL: cfLogo,
   });

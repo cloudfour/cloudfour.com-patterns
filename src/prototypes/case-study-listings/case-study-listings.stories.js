82ab5a144559f7a6ba2169a874a68606888b6dfa
Arianna Chau <ariannapikachau@gmail.com>
2021-06-21 16:39:02 -0700

    Replace book imagery

--- src/prototypes/case-study-listings/case-study-listings.stories.js
+++ src/prototypes/case-study-listings/case-study-listings.stories.js
@@ -2,7 +2,7 @@ import examplePrototype from './example/example.twig';
 import cpeImage from './images/collage-cpe.jpg';
 import wmImage from './images/walmart.jpg';
 import cfImage from './images/collage-cf.jpg';
-import bookApart from './images/bookapart.jpg';
+import bookApart from './images/aba.jpg';
 import codePiece from './images/code.jpg';
 import pwaStat from './images/pwastat.jpg';
 import responsiveImg from './images/responsiveimg.jpg';

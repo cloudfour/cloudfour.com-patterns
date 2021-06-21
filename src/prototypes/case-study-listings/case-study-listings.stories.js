38e2b2141796b6b8f22774bc1c3d7aa0b1c695ac
Arianna Chau <ariannapikachau@gmail.com>
2021-06-21 15:47:23 -0700

    Add new book image and open source section

--- src/prototypes/case-study-listings/case-study-listings.stories.js
+++ src/prototypes/case-study-listings/case-study-listings.stories.js
@@ -2,6 +2,11 @@ import examplePrototype from './example/example.twig';
 import cpeImage from './images/collage-cpe.jpg';
 import wmImage from './images/walmart.jpg';
 import cfImage from './images/collage-cf.jpg';
+import bookApart from './images/bookapart.jpg';
+import codePiece from './images/code.jpg';
+import pwaStat from './images/pwastat.jpg';
+import responsiveImg from './images/responsiveimg.jpg';
+import svgPlaceholder from './images/svgplaceholder.jpg';
 import './example/example.scss';
 
 export default {
@@ -17,4 +22,9 @@ export const Example = () =>
     cpe: cpeImage,
     wm: wmImage,
     cf: cfImage,
+    book: bookApart,
+    code: codePiece,
+    pwa: pwaStat,
+    responsive: responsiveImg,
+    svg: svgPlaceholder,
   });

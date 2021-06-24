52a5a9f62331e3c728425c1c2d1c7e5fe17c474f
Arianna Chau <ariannapikachau@gmail.com>
2021-06-24 11:28:18 -0700

    Prototypes/case study page (#1331)
    * Add case study listings prototype
    
    * Change slogan text
    
    * Adjust spacing
    
    * Remove logo and replace WM image
    
    * Change Ceasefire image
    
    * Adjust when title size increases
    
    * Add new book image and open source section
    
    * change title
    
    * Add new background color and padding
    
    * Replace book imagery
    
    * Update src/prototypes/case-study-listings/example/example.twig
    
    Co-authored-by: Tyler Sticka <tyler@cloudfour.com>
    
    * Update src/prototypes/case-study-listings/example/example.twig
    
    Co-authored-by: Tyler Sticka <tyler@cloudfour.com>
    
    * Add second book and adjust padding
    
    * Adjust padding and add new button
    
    * Increase padding
    
    Co-authored-by: Tyler Sticka <tyler@cloudfour.com>

---
+++ src/prototypes/case-study-listings/case-study-listings.stories.js
@@ -0,0 +1,32 @@
+import examplePrototype from './example/example.twig';
+import cpeImage from './images/collage-cpe.jpg';
+import wmImage from './images/walmart.jpg';
+import cfImage from './images/collage-cf.jpg';
+import bookApart from './images/aba.jpg';
+import mobileWeb from './images/mobile-web.jpg';
+import codePiece from './images/code.jpg';
+import pwaStat from './images/pwastat.jpg';
+import responsiveImg from './images/responsiveimg.jpg';
+import svgPlaceholder from './images/svgplaceholder.jpg';
+import './example/example.scss';
+
+export default {
+  title: 'Prototypes/Case Study Listings',
+  parameters: {
+    docs: { page: null },
+    paddings: { disable: true },
+  },
+};
+
+export const Example = () =>
+  examplePrototype({
+    cpe: cpeImage,
+    wm: wmImage,
+    cf: cfImage,
+    book: bookApart,
+    mobile: mobileWeb,
+    code: codePiece,
+    pwa: pwaStat,
+    responsive: responsiveImg,
+    svg: svgPlaceholder,
+  });

798db612eb1a39108b4520485bc7560acf39a6bd
Caleb Eby <caleb.eby01@gmail.com>
2021-07-07 11:17:09 -0700

    Fix remaining TS errors

--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,37 @@ declare module '*.twig' {
   function template(opts: Record<string, unknown>): string;
   export default template;
 }
+
+declare module 'jabber' {
+  class jabber {
+    /**
+     * @param themeWords Custom words that need to appear in some density
+     * @param themeWordDensity appearance of themeword 1 per this number so 5 will make it approx 1 per 5 words
+     * @param extraVowels additional vowel chars
+     * @param extraConsonants additional consonants
+     */
+    constructor(
+      themeWords?: string[],
+      themeWordDensity?: number,
+      extraVowels?: string,
+      extraConsonants?: string
+    );
+
+    /** Create email */
+    createEmail(customDomain?: string): string;
+
+    /** Create fake full name */
+    createFullName(salutation?: string): string;
+
+    /** Create paragraph of certain number of words */
+    createParagraph(length: number): string;
+
+    /** Create word of certain length */
+    createWord(
+      length: number,
+      capitalize?: boolean,
+      avoidThemeWords?: boolean
+    ): string;
+  }
+  export = jabber;
+}

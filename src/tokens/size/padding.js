03bcb864c136bfa1ba7aeb3863d331eebecbbd88
Tyler Sticka <tyler@tylersticka.com>
2021-07-08 10:02:24 -0700

    Convert container padding values to tokens

--- src/tokens/size/padding.js
+++ src/tokens/size/padding.js
@@ -1,4 +1,4 @@
-const { modularEm } = require('../../scripts/modular-scale');
+const { modularEm, modularRem } = require('../../scripts/modular-scale');
 
 module.exports = {
   size: {
@@ -15,6 +15,16 @@ module.exports = {
         vertical: {
           value: modularEm(-3),
           comment: 'Used for table cells or other repeating content rows.',
+        },
+      },
+      container: {
+        min: {
+          value: modularRem(-1),
+          comment: 'Minimum fluid padding for container objects.',
+        },
+        max: {
+          value: modularRem(3),
+          comment: 'Maximum fluid padding for container objects.',
         },
       },
     },

e7dd9da261e3987eb5f85c2aac682af643d1d193
Paul Hebert <paul@cloudfour.com>
2021-07-20 16:18:56 -0700

    Fix function name mismatch

--- src/components/comment/comment.ts
+++ src/components/comment/comment.ts
@@ -1,4 +1,4 @@
-export const initComments = () => {
+export const initCommentsWithRepliesEnabled = () => {
   const commentsWithRepliesEnabled = document.querySelectorAll(
     '.js-comment-with-replies-enabled'
   );

5f8f15e4a19d75ad61a071737ddb68914911ca7a
Paul Hebert <paul@cloudfour.com>
2021-07-20 14:31:02 -0700

    Add note about cancel button

--- src/components/comment/comment.ts
+++ src/components/comment/comment.ts
@@ -4,6 +4,12 @@ export const initComments = () => {
   for (const comment of [...comments]) {
     const replyButton = comment.querySelector('.js-comment-reply-button');
     const replyForm = comment.querySelector('.js-comment-reply-form');
+    // The cancel button is inside the Comment Form, so we need to reach inside
+    // that component to grab the button. This feels a little awkward since
+    // we're directly manipulating a child component, but this is a lot simpler
+    // than other potential solutions. Other options included:
+    // - Having the Comment Form emit a `cancel` event
+    // - Having the Comment Form have a block that you could pass a button into
     const cancelButton = comment.querySelector('.js-cancel-reply');
 
     replyButton?.addEventListener('click', () => {

2c53f3c604dad2eb617d9f6fc8bc3d30e3e5d9f2
Arianna Chau <ariannapikachau@gmail.com>
2021-07-28 15:01:02 -0700

    Add 3 team prototype pages

---
+++ src/prototypes/team/team.stories.js
@@ -0,0 +1,27 @@
+import teamListPrototype from './example/team-list.twig';
+import teamIndividualPrototype from './example/team-individual.twig';
+import teamArticlePage2 from './example/team-articles-page2.twig';
+import avatars from './data/avatars.json';
+import './example/team-list.scss';
+import './example/team-individual.scss';
+import './example/team-article-page2.scss';
+
+export default {
+  title: 'Prototypes/Team Page',
+  parameters: {
+    docs: { page: null },
+    paddings: { disable: true },
+  },
+};
+
+export const List = () =>
+  teamListPrototype({
+    avatars,
+  });
+
+export const IndividualBio = () =>
+  teamIndividualPrototype({
+    avatars,
+  });
+
+export const ArticlesPage2 = () => teamArticlePage2({});

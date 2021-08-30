c41239be95552e3bd2cb610fa5eb5d371f403bbc
Gerardo Rodriguez <mr.gerardo.rodriguez@gmail.com>
2021-08-30 15:40:43 -0700

    Add basic JS logic

---
+++ src/components/button-swap/button-swap.ts
@@ -0,0 +1,42 @@
+/**
+ * Button swap
+ */
+export const initButtonSwap = (buttonSwapEl: HTMLElement) => {
+  // The group wrappers
+  const subscribeGroup = buttonSwapEl.querySelector(
+    '.js-c-button-swap__subscribe-group'
+  ) as HTMLElement;
+  const unsubscribeGroup = buttonSwapEl.querySelector(
+    '.js-c-button-swap__unsubscribe-group'
+  ) as HTMLElement;
+  // The buttons
+  const subscribeBtn = subscribeGroup.querySelector(
+    '.js-c-button-swap__button'
+  ) as HTMLButtonElement;
+  const unsubscribeBtn = unsubscribeGroup.querySelector(
+    '.js-c-button-swap__button'
+  ) as HTMLButtonElement;
+
+  const onSubscribeClick = () => {
+    subscribeGroup.hidden = true;
+    unsubscribeGroup.hidden = false;
+  };
+
+  const onUnsubscribeClick = () => {
+    unsubscribeGroup.hidden = true;
+    subscribeGroup.hidden = false;
+  };
+
+  const destroy = () => {
+    // Clean up event listeners
+    subscribeBtn.removeEventListener('click', onSubscribeClick);
+    unsubscribeBtn.removeEventListener('click', onUnsubscribeClick);
+  };
+
+  // Intialize
+  subscribeBtn.addEventListener('click', onSubscribeClick);
+  unsubscribeBtn.addEventListener('click', onUnsubscribeClick);
+
+  // Return a public API for consumers of this component
+  return { destroy };
+};

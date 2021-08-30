f352fa0cdd0b870fca0c70a8086ac97ee4439b70
Gerardo Rodriguez <mr.gerardo.rodriguez@gmail.com>
2021-08-30 13:58:12 -0700

    Add initial test

--- src/components/button-swap/button-swap.test.ts
+++ src/components/button-swap/button-swap.test.ts
@@ -3,74 +3,78 @@ import type { ElementHandle, PleasantestUtils } from 'pleasantest';
 import { withBrowser } from 'pleasantest';
 import { loadTwigTemplate } from '../../../test-utils';
 
-const buttonMarkup = loadTwigTemplate(
+const buttonSwapMarkup = loadTwigTemplate(
   path.join(__dirname, './button-swap.twig')
 );
 // Helper to test the JS-driven toggling button
-const initButtonSwap = (
-  utils: PleasantestUtils,
-  togglingButton: ElementHandle
-) =>
+const initButtonSwap = (utils: PleasantestUtils, buttonSwapEl: ElementHandle) =>
   utils.runJS(
     `
-    import { initTogglingButton } from './toggling-button'
-    export default (togglingButton) => initTogglingButton(togglingButton)
+    import { initButtonSwap } from './button-swap'
+    export default (buttonSwapEl) => initButtonSwap(buttonSwapEl)
     `,
-    [togglingButton]
+    [buttonSwapEl]
   );
 
 test(
-  'should render a button element by default',
+  'initial setup',
   withBrowser(async ({ utils, screen }) => {
-    await utils.injectHTML(await buttonMarkup());
+    await utils.injectHTML(await buttonSwapMarkup());
 
-    const button = await screen.getByRole('button');
-    await expect(button).toHaveTextContent(/^hello world$/i);
+    const subscribeBtn = await screen.getByRole('button', {
+      name: /get notifications/i,
+    });
+    const unsubscribeBtn = await screen.getByRole('button', {
+      name: /turn off notifications/i,
+    });
+
+    await expect(subscribeBtn).toBeInTheDocument();
+    await expect(unsubscribeBtn).not.toBeInTheDocument();
   })
 );
 
-test(
-  'should render an anchor element when href is provided',
-  withBrowser(async ({ utils, screen }) => {
-    await utils.injectHTML(await buttonMarkup({ href: '#' }));
+// test(
+//   'should render an anchor element when href is provided',
+//   withBrowser(async ({ utils, screen }) => {
+//     await utils.injectHTML(await buttonSwapMarkup({ href: '#' }));
 
-    const linkButton = await screen.getByRole('link');
-    await expect(linkButton).toHaveTextContent(/^hello world$/i);
-  })
-);
+//     const linkButton = await screen.getByRole('link');
+//     await expect(linkButton).toHaveTextContent(/^hello world$/i);
+//   })
+// );
 
-test(
-  'should render a button when aria_pressed is set',
-  withBrowser(async ({ utils, screen }) => {
-    await utils.injectHTML(
-      // The `aria_pressed` value should override the `tag_name` value and always
-      // render a button element. This test verifies that expecation.
-      await buttonMarkup({
-        tag_name: 'a',
-        aria_pressed: 'false',
-        label: 'I am a button',
-      })
-    );
+// test(
+//   'should render a button when aria_pressed is set',
+//   withBrowser(async ({ utils, screen }) => {
+//     await utils.injectHTML(
+//       // The `aria_pressed` value should override the `tag_name` value and always
+//       // render a button element. This test verifies that expecation.
+//       await buttonSwapMarkup({
+//         tag_name: 'a',
+//         aria_pressed: 'false',
+//         label: 'I am a button',
+//       })
+//     );
 
-    const button = await screen.getByRole('button');
-    await expect(button).toHaveTextContent(/^i am a button$/i);
-  })
-);
+//     const button = await screen.getByRole('button');
+//     await expect(button).toHaveTextContent(/^i am a button$/i);
+//   })
+// );
 
-test(
-  'should toggle aria-pressed state',
-  withBrowser(async ({ utils, screen, user }) => {
-    await utils.injectHTML(await buttonMarkup({ aria_pressed: 'false' }));
+// test(
+//   'should toggle aria-pressed state',
+//   withBrowser(async ({ utils, screen, user }) => {
+//     await utils.injectHTML(await buttonSwapMarkup({ aria_pressed: 'false' }));
 
-    const togglingButton = await screen.getByRole('button');
-    await initButtonSwap(utils, togglingButton);
+//     const togglingButton = await screen.getByRole('button');
+//     await initButtonSwap(utils, togglingButton);
 
-    await expect(togglingButton).toHaveAttribute('aria-pressed', 'false');
+//     await expect(togglingButton).toHaveAttribute('aria-pressed', 'false');
 
-    await user.click(togglingButton);
-    await expect(togglingButton).toHaveAttribute('aria-pressed', 'true');
+//     await user.click(togglingButton);
+//     await expect(togglingButton).toHaveAttribute('aria-pressed', 'true');
 
-    await user.click(togglingButton);
-    await expect(togglingButton).toHaveAttribute('aria-pressed', 'false');
-  })
-);
+//     await user.click(togglingButton);
+//     await expect(togglingButton).toHaveAttribute('aria-pressed', 'false');
+//   })
+// );

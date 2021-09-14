b1bbff485aa6d171c35811282efa930949e66761
Gerardo Rodriguez <gerardo@cloudfour.com>
2021-09-13 17:02:43 -0700

    Button Swap component (#1516)
    Adds a Button Swap component with the intent to be used to subscribe/unsubscribe from push notifications on article pages.

---
+++ src/components/button-swap/button-swap.test.ts
@@ -0,0 +1,176 @@
+import path from 'path';
+import type { PleasantestUtils } from 'pleasantest';
+import { withBrowser } from 'pleasantest';
+import { loadTwigTemplate, loadGlobalCSS } from '../../../test-utils';
+
+// Helper to load the Twig template file
+const componentMarkup = loadTwigTemplate(
+  path.join(__dirname, './button-swap.twig')
+);
+
+// Helper to initialize the component JS
+const initJS = (
+  utils: PleasantestUtils,
+  initialCallback?: () => void,
+  swappedCallback?: () => void
+) =>
+  utils.runJS(
+    `
+    import { initButtonSwap } from './button-swap'
+    export default (initialCallback, swappedCallback) => {
+      initButtonSwap(document.querySelector('.js-button-swap'), {
+        initialCallback,
+        swappedCallback
+      })
+    }
+    `,
+    [initialCallback, swappedCallback]
+  );
+
+test(
+  'Initial state',
+  withBrowser(async ({ utils, screen }) => {
+    await utils.injectHTML(await componentMarkup());
+    await loadGlobalCSS(utils);
+
+    // Visually hidden text for a more inclusive UX
+    const statusMsg = await screen.getByRole('status');
+    await expect(statusMsg).toHaveTextContent(
+      /^currently unsubscribed from notifications$/i
+    );
+
+    // Confirm only one is accessible, throws if more than one exists
+    // https://testing-library.com/docs/dom-testing-library/cheatsheet/#queries
+    await screen.getByRole('status');
+
+    const firstBtn = await screen.getByRole('button', {
+      name: /^get notifications$/i,
+    });
+    await expect(firstBtn).toBeVisible();
+
+    const secondBtn = await screen.queryByRole('button', {
+      name: /^turn off notifications$/i,
+    });
+    expect(secondBtn).toBeNull();
+  })
+);
+
+test(
+  'Swap UI state when clicked',
+  withBrowser(async ({ utils, screen, user }) => {
+    await utils.injectHTML(await componentMarkup());
+    await loadGlobalCSS(utils);
+    await initJS(utils);
+
+    let firstBtn = await screen.queryByRole('button', {
+      name: /^get notifications$/i,
+    });
+    await expect(firstBtn).not.toHaveClass('is-slashed');
+
+    // Button swap action
+    await user.click(firstBtn);
+
+    // Visually hidden text for a more inclusive UX
+    let statusMsg = await screen.getByRole('alert');
+    await expect(statusMsg).toHaveTextContent(
+      /^currently subscribed to notifications$/i
+    );
+    await expect(statusMsg).toHaveFocus();
+
+    // Query for first button again in its new state
+    firstBtn = await screen.queryByRole('button', {
+      name: /^get notifications$/i,
+    });
+    expect(firstBtn).toBeNull();
+
+    let secondBtn = await screen.queryByRole('button', {
+      name: /^turn off notifications$/i,
+    });
+    await expect(secondBtn).toBeVisible();
+    await expect(secondBtn).toHaveClass('is-slashed');
+
+    // Button swap action
+    await user.click(secondBtn);
+
+    // Visually hidden text for a more inclusive UX
+    statusMsg = await screen.getByRole('alert');
+    await expect(statusMsg).toHaveTextContent(
+      /^currently unsubscribed from notifications$/i
+    );
+    await expect(statusMsg).toHaveFocus();
+
+    // Query for second button again in its new state
+    secondBtn = await screen.queryByRole('button', {
+      name: /^turn off notifications$/i,
+    });
+    expect(secondBtn).toBeNull();
+
+    // Query for first button again in its new state
+    firstBtn = await screen.queryByRole('button', {
+      name: /^get notifications$/i,
+    });
+    await expect(firstBtn).toBeVisible();
+    await expect(firstBtn).not.toHaveClass('is-slashed');
+  })
+);
+
+test(
+  'Set custom messages and labels',
+  withBrowser(async ({ utils, screen, user }) => {
+    await utils.injectHTML(
+      await componentMarkup({
+        initial_visual_label: 'Hello world',
+        swapped_visual_label: 'Have a great day',
+        initial_label: 'Unsubscribed',
+        swapped_label: 'Subscribed',
+      })
+    );
+    await loadGlobalCSS(utils);
+    await initJS(utils);
+
+    // Visually hidden text for a more inclusive UX
+    let statusMsg = await screen.getByRole('status');
+    await expect(statusMsg).toHaveTextContent('Unsubscribed');
+
+    const firstBtn = await screen.getByRole('button');
+    await expect(firstBtn).toHaveTextContent('Hello world');
+
+    // Button swap action
+    await user.click(firstBtn);
+
+    // Visually hidden text for a more inclusive UX
+    statusMsg = await screen.getByRole('alert');
+    await expect(statusMsg).toHaveTextContent('Subscribed');
+
+    const secondBtn = await screen.queryByRole('button');
+    await expect(secondBtn).toHaveTextContent('Have a great day');
+  })
+);
+
+test(
+  'Callback functions are called',
+  withBrowser(async ({ utils, screen, user }) => {
+    await utils.injectHTML(await componentMarkup());
+
+    const mockInitialCallback = jest.fn();
+    const mockSwappedCallback = jest.fn();
+
+    await initJS(utils, mockInitialCallback, mockSwappedCallback);
+
+    const firstBtn = await screen.queryByRole('button', {
+      name: /^get notifications$/i,
+    });
+
+    await user.click(firstBtn);
+
+    expect(mockInitialCallback).toBeCalledTimes(1);
+
+    const secondBtn = await screen.queryByRole('button', {
+      name: /^turn off notifications$/i,
+    });
+
+    await user.click(secondBtn);
+
+    expect(mockSwappedCallback).toBeCalledTimes(1);
+  })
+);
---
+++ src/components/button-swap/button-swap.ts
@@ -0,0 +1,96 @@
+/**
+ * Helper to get the button from the supplied wrapper element
+ */
+const getButton = (wrapper: HTMLElement) =>
+  wrapper.querySelector('.js-button-swap__button') as HTMLButtonElement;
+
+/**
+ * Helper to get the status message from the supplied wrapper element
+ */
+const getStatusMessage = (wrapper: HTMLElement) =>
+  wrapper.querySelector('.js-button-swap__message') as HTMLElement;
+
+/**
+ * Performs click actions
+ */
+const onClick = ({
+  event,
+  hiddenWrapper,
+  visibleWrapper,
+  callback,
+}: {
+  event: Event;
+  hiddenWrapper: HTMLElement;
+  visibleWrapper: HTMLElement;
+  callback: (event: Event) => void;
+}) => {
+  hiddenWrapper.hidden = true;
+  visibleWrapper.hidden = false;
+
+  const statusMsg = getStatusMessage(visibleWrapper);
+  statusMsg.setAttribute('role', 'alert');
+  statusMsg.focus();
+
+  callback(event);
+};
+
+/**
+ * Button swap
+ *
+ * Swaps two buttons by toggling the `hidden` attribute on the wrapper for each
+ * button + visually hidden message group; only one button + visually hidden
+ * message is displayed at a time.
+ */
+export const initButtonSwap = (
+  buttonSwapEl: HTMLElement,
+  {
+    initialCallback,
+    swappedCallback,
+  }: {
+    initialCallback: (event: Event) => void;
+    swappedCallback: (event: Event) => void;
+  }
+) => {
+  // The group wrappers
+  const initialButtonWrapper = buttonSwapEl.querySelector(
+    '.js-button-swap__initial-button-wrapper'
+  ) as HTMLElement;
+  const swappedButtonWrapper = buttonSwapEl.querySelector(
+    '.js-button-swap__swapped-button-wrapper'
+  ) as HTMLElement;
+
+  // The buttons
+  const initialButton = getButton(initialButtonWrapper);
+  const swappedButton = getButton(swappedButtonWrapper);
+
+  const onInitialButtonClick = (event: Event) => {
+    onClick({
+      event,
+      hiddenWrapper: initialButtonWrapper,
+      visibleWrapper: swappedButtonWrapper,
+      callback: initialCallback,
+    });
+  };
+
+  const onSwappedButtonClick = (event: Event) => {
+    onClick({
+      event,
+      hiddenWrapper: swappedButtonWrapper,
+      visibleWrapper: initialButtonWrapper,
+      callback: swappedCallback,
+    });
+  };
+
+  const destroy = () => {
+    // Clean up event listeners
+    initialButton.removeEventListener('click', onInitialButtonClick);
+    swappedButton.removeEventListener('click', onSwappedButtonClick);
+  };
+
+  // Intialize
+  initialButton.addEventListener('click', onInitialButtonClick);
+  swappedButton.addEventListener('click', onSwappedButtonClick);
+
+  // Return a public API for consumers of this component
+  return { destroy };
+};

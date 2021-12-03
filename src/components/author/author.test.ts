e6a86975f1c0a4f1a90bff1443fc84bba849b5c2
Gerardo Rodriguez <mr.gerardo.rodriguez@gmail.com>
2021-12-02 17:04:37 -0800

    PR feedback

--- src/components/author/author.test.ts
+++ src/components/author/author.test.ts
@@ -7,139 +7,55 @@ const template = loadTwigTemplate(path.join(__dirname, './author.twig'));
 
 describe('Author', () => {
   test(
-    'single author with a date',
+    'more accessible experience for publish date',
     withBrowser(async ({ utils, screen }) => {
       await utils.injectHTML(
         await template({
-          // The avatars are not included because I couldn't figure out how
-          // to include them. For the purposes of these tests, though, it's
-          // not important so I left them out.
+          // The avatar is not included because I couldn't figure out how
+          // to include it. For the purposes of this test, though, it is
+          // not important so I left it out.
           authors: [
             {
-              name: 'Megan Notarte',
-              link: 'https://cloudfour.com/is/megnotarte',
-              job_title: 'Partner, Vice President',
+              name: 'Shakira Isabel Mebarak Ripoll',
+              link: 'https://www.shakira.com/',
             },
           ],
           date: new Date('March 31, 2021'),
         })
       );
 
-      // The date should render
-      const publishDate = await screen.queryByText(
-        /published\son\s\w*\s\d{1,2}\D*,\s\d{2,4}/i
-      );
-      expect(publishDate).not.toBeNull();
-
-      // The job title should not render when a date is provided
-      const jobTitle = await screen.queryByText('Partner, Vice President');
-      expect(jobTitle).toBeNull();
+      // Confirm a more accessible user experience
+      await screen.getByText('Published on March 31st, 2021');
     })
   );
 
   test(
-    'single author without a date',
+    'meta is prioritized over date',
     withBrowser(async ({ utils, screen }) => {
       await utils.injectHTML(
         await template({
-          // The avatars are not included because I couldn't figure out how
-          // to include them. For the purposes of these tests, though, it's
-          // not important so I left them out.
+          // The avatar is not included because I couldn't figure out how
+          // to include it. For the purposes of this test, though, it is
+          // not important so I left it out.
           authors: [
             {
-              name: 'Megan Notarte',
-              link: 'https://cloudfour.com/is/megnotarte',
-              job_title: 'Partner, Vice President',
-            },
-          ],
-        })
-      );
-
-      // The date should not render
-      const publishDate = await screen.queryByText(
-        /published\son\s\w*\s\d{1,2}\D*,\s\d{2,4}/i
-      );
-      expect(publishDate).toBeNull();
-
-      // The job title should render when a date is not provided
-      const jobTitle = await screen.queryByText('Partner, Vice President');
-      expect(jobTitle).not.toBeNull();
-    })
-  );
-
-  test(
-    'multiple authors with a date',
-    withBrowser(async ({ utils, screen }) => {
-      await utils.injectHTML(
-        await template({
-          // The avatars are not included because I couldn't figure out how
-          // to include them. For the purposes of these tests, though, it's
-          // not important so I left them out.
-          authors: [
-            {
-              name: 'Megan Notarte',
-              link: 'https://cloudfour.com/is/megnotarte',
-              job_title: 'Partner, Vice President',
-            },
-            {
-              name: 'Tyler Sticka',
-              link: 'https://cloudfour.com/is/tyler',
-              job_title: 'Partner, Vice President',
+              name: 'Shakira Isabel Mebarak Ripoll',
+              link: 'https://www.shakira.com/',
             },
           ],
           date: new Date('March 31, 2021'),
+          meta: 'Singer and songwriter',
         })
       );
 
-      // The date should render
+      // Confirm the meta value is rendered
+      await screen.getByText('Singer and songwriter');
+
+      // Confirm the date is not rendered
       const publishDate = await screen.queryByText(
-        /published\son\s\w*\s\d{1,2}\D*,\s\d{2,4}/i
-      );
-      expect(publishDate).not.toBeNull();
-
-      // The job title should not render when multiple authors are provided
-      const jobTitle = await screen.queryByText('Partner, Vice President');
-      expect(jobTitle).toBeNull();
-    })
-  );
-
-  test(
-    'multiple authors without a date',
-    withBrowser(async ({ utils, screen }) => {
-      await utils.injectHTML(
-        await template({
-          // The avatars are not included because I couldn't figure out how
-          // to include them. For the purposes of these tests, though, it's
-          // not important so I left them out.
-          authors: [
-            {
-              name: 'Megan Notarte',
-              link: 'https://cloudfour.com/is/megnotarte',
-              job_title: 'Partner, Vice President',
-            },
-            {
-              name: 'Tyler Sticka',
-              link: 'https://cloudfour.com/is/tyler',
-              job_title: 'Partner, Vice President',
-            },
-            {
-              name: 'Gerardo Rodriguez',
-              link: 'https://cloudfour.com/is/gerardo',
-              job_title: 'Senior Front-end Developer',
-            },
-          ],
-        })
-      );
-
-      // The date should not render
-      const publishDate = await screen.queryByText(
-        /published\son\s\w*\s\d{1,2}\D*,\s\d{2,4}/i
+        'Published on March 31st, 2021'
       );
       expect(publishDate).toBeNull();
-
-      // The job title should not render when multiple authors are provided
-      const jobTitle = await screen.queryByText('Partner, Vice President');
-      expect(jobTitle).toBeNull();
     })
   );
 });

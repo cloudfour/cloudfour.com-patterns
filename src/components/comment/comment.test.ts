18411b895fca8aafa3302fc05688a5adce1bef7c
Caleb Eby <calebeby@users.noreply.github.com>
2021-12-23 13:33:42 -0800

    Update pleasantest and use getAccessibilityTree in tests (#1619)

--- src/components/comment/comment.test.ts
+++ src/components/comment/comment.test.ts
@@ -1,10 +1,10 @@
 import path from 'path';
-import type { PleasantestUtils } from 'pleasantest';
-import { withBrowser } from 'pleasantest';
+import type { ElementHandle, PleasantestUtils } from 'pleasantest';
+import { withBrowser, getAccessibilityTree } from 'pleasantest';
 import { loadTwigTemplate, loadGlobalCSS } from '../../../test-utils';
 
 const commentMarkup = loadTwigTemplate(path.join(__dirname, 'comment.twig'));
-const initCommentsJs = (utils: PleasantestUtils) =>
+const initCommentsJS = (utils: PleasantestUtils) =>
   utils.runJS(`
     import { initCommentReplyForm } from './comment';
 
@@ -13,18 +13,18 @@ const initCommentsJs = (utils: PleasantestUtils) =>
 
 test(
   'reply form can be opened and closed',
-  withBrowser(async ({ utils, screen, user }) => {
+  withBrowser(async ({ utils, screen, user, page }) => {
     await utils.injectHTML(
       await commentMarkup({
         comment: {
           ID: 1,
           link: '#comment-1',
-          date: new Date(),
+          date: new Date('January 1 2000'),
           avatar: '',
           author: {
-            name: 'Test',
+            name: 'Test author name',
           },
-          comment_content: 'Test',
+          comment_content: 'Test content',
           approved: true,
         },
         allow_replies: true,
@@ -33,8 +33,18 @@ test(
 
     await loadGlobalCSS(utils);
 
-    await initCommentsJs(utils);
+    await initCommentsJS(utils);
 
+    const body = await page.evaluateHandle<ElementHandle>(() => document.body);
+    expect(await getAccessibilityTree(body, { includeText: false }))
+      .toMatchInlineSnapshot(`
+        article "Test author name said:"
+          banner
+            heading "Test author name said:"
+          contentinfo
+            link "Permalink to Test author name’s Jan 1, 2000 comment"
+            button "Reply"
+      `);
     const form = await screen.getByRole('form', { hidden: true });
     const replyButton = await screen.getByRole('button', {
       name: /reply/i,
@@ -49,12 +59,20 @@ test(
     await expect(form).toBeVisible();
     // Reply button is hidden
     await expect(replyButton).not.toBeVisible();
-    // The first input or textarea should be focused.
-    // (In practice this will always be a textarea, but if the form is
-    // changed to lead with an input in the future, and that input is selected,
-    // this test should still pass.)
-    const inputs = await screen.getAllByRole(/textbox|input/);
-    await expect(inputs[0]).toHaveFocus();
+    // Check that all the fields appear and have correct labels and the first textbox should be focused
+    expect(await getAccessibilityTree(form, { includeText: false }))
+      .toMatchInlineSnapshot(`
+        form "Reply to Test author name"
+          heading "Reply to Test author name"
+          link "Markdown"
+          textbox "Reply" (focused)
+            ↳ description: "Please be kind, courteous and constructive. You may use simple HTML or Markdown in your comments. All fields are required."
+          textbox "Name"
+          textbox "Email"
+          checkbox "Notify me of follow-up comments by email."
+          button "Submit Reply"
+          button "Cancel"
+    `);
 
     // Click the cancel button to get back to our initial state
     const cancelButton = await screen.getByRole('button', {

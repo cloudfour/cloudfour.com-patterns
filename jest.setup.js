74aeeca802e55d4a2be84b3be55c73f074ad48f5
Caleb Eby <calebeby@users.noreply.github.com>
2022-03-31 15:46:20 -0700

    Update jest.setup.js
    Co-authored-by: Gerardo Rodriguez <gerardo@cloudfour.com>

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,7 @@
 import { configureDefaults } from 'pleasantest';
 
 configureDefaults({
+  // Design intent doesn't require Level AAA conformance, 
+  // but we want to keep a baseline targetSize check
   user: { targetSize: 35 /* px */ },
 });

d8508b119cd92797df4fc9d58ff3dc96ab25a2bc
Scott Vandehey <scott@cloudfour.com>
2022-04-14 11:16:57 -0700

    Add Short Date Format and Unlinking to Author (#1727)
    This PR adds two new features to the Author component. First, it adds
    the `unlink` property, which will prevent a link from being rendered
    even if one is provided as part of the Author object. This is useful
    for situations where removing the link property from the Author is
    a hassle. Second, it adds a `date_format` property, which can be set
    to `short` to use abbreviate date formatting.

---
+++ .changeset/eighty-berries-knock.md
@@ -0,0 +1,5 @@
+---
+'@cloudfour/patterns': minor
+---
+
+Add Short Date Format and Unlinking to Author
--- src/components/author/author.test.ts
+++ src/components/author/author.test.ts
@@ -36,6 +36,31 @@ test(
 );
 
 test(
+  'Short date formatting',
+  withBrowser(async ({ utils, screen }) => {
+    await utils.injectHTML(
+      await template({
+        // The avatar is not included because I couldn't figure out how
+        // to include it. For the purposes of this test, though, it is
+        // not important so I left it out.
+        authors: [
+          {
+            name: 'Shakira Isabel Mebarak Ripoll',
+          },
+        ],
+        date: new Date('March 31, 2021'),
+        date_format: 'short',
+      })
+    );
+
+    // The short date formatting applies to the visible date text
+    // (not our visually hidden screen reader text)
+    const visibleDateText = await screen.getByText('Mar 31, 2021');
+    await expect(visibleDateText).toHaveAttribute('aria-hidden', 'true');
+  })
+);
+
+test(
   'meta is prioritized over date',
   withBrowser(async ({ utils, page }) => {
     await utils.injectHTML(
@@ -79,6 +104,34 @@ test(
             name: 'Shakira Isabel Mebarak Ripoll',
           },
         ],
+      })
+    );
+
+    const body = await page.evaluateHandle<ElementHandle>(() => document.body);
+
+    // Confirm the author name is "text" and not a link
+    expect(await getAccessibilityTree(body)).toMatchInlineSnapshot(`
+      text "By"
+      text "Shakira Isabel Mebarak Ripoll"
+    `);
+  })
+);
+
+test(
+  'Can remove author link prop',
+  withBrowser(async ({ utils, page }) => {
+    await utils.injectHTML(
+      await template({
+        // The avatar is not included because I couldn't figure out how
+        // to include it. For the purposes of this test, though, it is
+        // not important so I left it out.
+        authors: [
+          {
+            name: 'Shakira Isabel Mebarak Ripoll',
+            link: 'https://www.shakira.com/',
+          },
+        ],
+        unlink: true,
       })
     );
 

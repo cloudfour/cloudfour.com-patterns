5502b9f36fdef1ca214551ac5387873d93d3414d
Tyler Sticka <tyler@cloudfour.com>
2022-04-18 16:37:16 -0700

    Increase container block/vertical padding (#1740)

--- src/tokens/size/padding.js
+++ src/tokens/size/padding.js
@@ -18,13 +18,28 @@ module.exports = {
         },
       },
       container: {
-        min: {
-          value: modularRem(-1),
-          comment: 'Minimum fluid padding for container objects.',
+        horizontal: {
+          min: {
+            value: modularRem(-1),
+            comment:
+              'Minimum inline (horizontal) fluid padding for container objects.',
+          },
+          max: {
+            value: modularRem(3),
+            comment:
+              'Maximum inline (horizontal) padding for container objects.',
+          },
         },
-        max: {
-          value: modularRem(3),
-          comment: 'Maximum fluid padding for container objects.',
+        vertical: {
+          min: {
+            value: modularRem(3),
+            comment:
+              'Minimum block (vertical) fluid padding for container objects.',
+          },
+          max: {
+            value: modularRem(6),
+            comment: 'Maximum block (vertical) padding for container objects.',
+          },
         },
       },
     },

f0febd4b7cc9f22564d51abafa35cfa70f4aabad
Caleb Eby <caleb.eby01@gmail.com>
2022-05-27 16:22:01 -0700

    Disable colormin via preset options

--- gulpfile.js/tasks/build-sass.js
+++ gulpfile.js/tasks/build-sass.js
@@ -17,11 +17,13 @@ const buildSass = () =>
     .pipe(postcss())
     .pipe(rename({ basename: 'standalone' }))
     .pipe(dest(outDir))
-    .pipe(postcss([cssnano()]))
-    // Cssnano minimizes alpha colors to 8-digit hex codes, which is still stage 2 and is not supported in some browsers
-    // There didn't seem to be an easy way to disable this minification within cssnano,
-    // and it doesn't seem to work to add the postcss-color-hex-alpha plugin in the same postcss pass as cssnano
-    .pipe(postcss([require('postcss-color-hex-alpha')()]))
+    .pipe(
+      postcss([
+        cssnano({
+          preset: ['cssnano-preset-default', { colormin: false }],
+        }),
+      ])
+    )
     .pipe(rename({ extname: '.min.css' }))
     .pipe(dest(outDir));
 

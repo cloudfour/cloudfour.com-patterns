9363f95a60289ff0bbd33f822d91409194b4faf1
Scott Vandehey <scott@cloudfour.com>
2022-06-14 16:32:19 -0700

    tweaks

--- src/tokens/size/rhythm.js
+++ src/tokens/size/rhythm.js
@@ -1,4 +1,4 @@
-const { modularEm } = require('../../scripts/modular-scale');
+const { modularEm, modularRem } = require('../../scripts/modular-scale');
 
 module.exports = {
   size: {
@@ -6,6 +6,7 @@ module.exports = {
       compact: { value: modularEm(-6) },
       condensed: { value: modularEm(-1) },
       default: { value: modularEm(1) },
+      default_rem: { value: modularRem(1) },
       generous: { value: modularEm(3) },
     },
   },

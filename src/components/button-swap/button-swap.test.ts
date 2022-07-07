4e8ef0449aaa446392777df0c27b33d48fab7ba2
Caleb Eby <calebeby@users.noreply.github.com>
2022-07-07 13:01:48 -0700

    Remove button swap JS (#1917)

--- src/components/button-swap/button-swap.test.ts
+++ src/components/button-swap/button-swap.test.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 
-import type { ElementHandle, PleasantestUtils } from 'pleasantest';
+import type { ElementHandle } from 'pleasantest';
 import { getAccessibilityTree, withBrowser } from 'pleasantest';
 
 import { loadGlobalCSS, loadTwigTemplate } from '../../../test-utils.js';
@@ -10,28 +10,9 @@ const componentMarkup = loadTwigTemplate(
   path.join(__dirname, './button-swap.twig')
 );
 
-// Helper to initialize the component JS
-const initJS = (
-  utils: PleasantestUtils,
-  initialCallback?: () => void,
-  swappedCallback?: () => void
-) =>
-  utils.runJS(
-    `
-    import { initButtonSwap } from './button-swap'
-    export default (initialCallback, swappedCallback) => {
-      initButtonSwap(document.querySelector('.js-button-swap'), {
-        initialCallback,
-        swappedCallback
-      })
-    }
-    `,
-    [initialCallback, swappedCallback]
-  );
-
 test(
   'Swap UI state when clicked',
-  withBrowser(async ({ utils, screen, user, page }) => {
+  withBrowser(async ({ utils, screen, page }) => {
     await utils.injectHTML(await componentMarkup());
     await loadGlobalCSS(utils);
 
@@ -47,8 +28,6 @@ test(
     await expect(firstBtn).toBeVisible();
     await expect(firstBtn).not.toHaveClass('is-slashed');
 
-    await initJS(utils);
-
     // After JS initializes it should be the same
     expect(await getAccessibilityTree(body)).toMatchInlineSnapshot(`
       status
@@ -59,41 +38,12 @@ test(
     firstBtn = await screen.queryByRole('button');
     await expect(firstBtn).toBeVisible();
     await expect(firstBtn).not.toHaveClass('is-slashed');
-
-    // Button swap action
-    await user.click(firstBtn);
-
-    expect(await getAccessibilityTree(body)).toMatchInlineSnapshot(`
-      alert (focused)
-        text "Currently subscribed to notifications"
-      button "Turn off notifications"
-    `);
-
-    const secondBtn = await screen.queryByRole('button', {
-      name: /^turn off notifications$/i,
-    });
-    await expect(secondBtn).toBeVisible();
-    await expect(secondBtn).toHaveClass('is-slashed');
-
-    // Button swap action
-    await user.click(secondBtn);
-
-    expect(await getAccessibilityTree(body)).toMatchInlineSnapshot(`
-      alert (focused)
-        text "Currently unsubscribed from notifications"
-      button "Get notifications"
-    `);
-
-    // Query for first button again in its new state
-    firstBtn = await screen.queryByRole('button');
-    await expect(firstBtn).toBeVisible();
-    await expect(firstBtn).not.toHaveClass('is-slashed');
   })
 );
 
 test(
   'Set custom messages and labels',
-  withBrowser(async ({ utils, screen, user, page }) => {
+  withBrowser(async ({ utils, page }) => {
     await utils.injectHTML(
       await componentMarkup({
         initial_visual_label: 'Hello world',
@@ -103,7 +53,6 @@ test(
       })
     );
     await loadGlobalCSS(utils);
-    await initJS(utils);
 
     const body = await page.evaluateHandle<ElementHandle>(() => document.body);
     expect(await getAccessibilityTree(body)).toMatchInlineSnapshot(`
@@ -111,43 +60,5 @@ test(
         text "Unsubscribed"
       button "Hello world"
     `);
-
-    // Button swap action
-    await user.click(await screen.getByRole('button'));
-
-    expect(await getAccessibilityTree(body)).toMatchInlineSnapshot(`
-      alert (focused)
-        text "Subscribed"
-      button "Have a great day"
-    `);
-  })
-);
-
-test(
-  'Callback functions are called',
-  withBrowser(async ({ utils, screen, user }) => {
-    await utils.injectHTML(await componentMarkup());
-    await loadGlobalCSS(utils);
-
-    const mockInitialCallback = jest.fn();
-    const mockSwappedCallback = jest.fn();
-
-    await initJS(utils, mockInitialCallback, mockSwappedCallback);
-
-    const firstBtn = await screen.queryByRole('button', {
-      name: /^get notifications$/i,
-    });
-
-    await user.click(firstBtn);
-
-    expect(mockInitialCallback).toBeCalledTimes(1);
-
-    const secondBtn = await screen.queryByRole('button', {
-      name: /^turn off notifications$/i,
-    });
-
-    await user.click(secondBtn);
-
-    expect(mockSwappedCallback).toBeCalledTimes(1);
   })
 );

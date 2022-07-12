4708f00a225f54d60d1860512cdd08a92f621023
Scott Vandehey <scott@cloudfour.com>
2022-07-12 16:19:27 -0700

    Add `is_sectioning` check to Card and Overview (#1936)
    This PR updates the template for the Card component and Overview object
    to check if `tag_name` is a sectioning element like `article` or
    `section`, and then change the default for `header_tag_element` and
    `footer_tag_element` to either `header`/`footer` or `div`.
    
    This avoids an issue when `header` elements are rendered in a `div`,
    it causes useless "banner" landmarks to display in the VO Rotor.
    
    Fixes #1934

---
+++ src/components/card/card.test.ts
@@ -0,0 +1,54 @@
+import path from 'path';
+
+import type { ElementHandle } from 'pleasantest';
+import { getAccessibilityTree, withBrowser } from 'pleasantest';
+
+import { loadTwigTemplate } from '../../../test-utils.js';
+
+/** Helper to load the Twig template file */
+const template = loadTwigTemplate(path.join(__dirname, './demo/single.twig'));
+const divTemplate = loadTwigTemplate(path.join(__dirname, './demo/div.twig'));
+
+describe('Card component', () => {
+  test(
+    'should use header/footer with article',
+    withBrowser(async ({ utils, page }) => {
+      await utils.injectHTML(
+        await template({
+          show_heading: true,
+          show_footer: true,
+        })
+      );
+
+      const body = await page.evaluateHandle<ElementHandle>(
+        () => document.body
+      );
+      expect(await getAccessibilityTree(body, { includeText: false }))
+        .toMatchInlineSnapshot(`
+          article
+            banner
+              heading "Lorem ipsum dolor sit amet" (level=2)
+            contentinfo
+        `);
+    })
+  );
+
+  test(
+    'should not use header/footer with div',
+    withBrowser(async ({ utils, page }) => {
+      await utils.injectHTML(
+        await divTemplate({
+          show_heading: true,
+          show_footer: true,
+        })
+      );
+
+      const body = await page.evaluateHandle<ElementHandle>(
+        () => document.body
+      );
+      expect(
+        await getAccessibilityTree(body, { includeText: false })
+      ).toMatchInlineSnapshot(`heading "Lorem ipsum dolor sit amet" (level=2)`);
+    })
+  );
+});

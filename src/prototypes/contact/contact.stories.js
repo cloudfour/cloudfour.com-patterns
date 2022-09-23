fe9e7581c748e52f7b1342797367042db6895fa1
Tyler Sticka <tyler@cloudfour.com>
2022-09-23 10:29:25 -0700

    Add second contact prototype

--- src/prototypes/contact/contact.stories.js
+++ src/prototypes/contact/contact.stories.js
@@ -3,7 +3,9 @@ import { useEffect } from '@storybook/client-api';
 import { createElasticTextArea } from '../../components/input/elastic-textarea.ts';
 
 import contactA from './contact-a.twig';
+import contactB from './contact-b.twig';
 import './contact-a.scss';
+import './contact-b.scss';
 
 export default {
   title: 'Prototypes/Contact',
@@ -18,4 +20,11 @@ export const ContactA = () => {
     createElasticTextArea(document.querySelector('.js-elastic-textarea'));
   }, []);
   return contactA();
+};
+
+export const ContactB = () => {
+  useEffect(() => {
+    createElasticTextArea(document.querySelector('.js-elastic-textarea'));
+  }, []);
+  return contactB();
 };

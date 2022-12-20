a8e47d34ee988f7c9df4884b154d314d9e35f2c0
Gerardo Rodriguez <mr.gerardo.rodriguez@gmail.com>
2022-12-20 06:40:24 -0800

    Add a Draft Email prototype

---
+++ src/prototypes/draft-email-textarea/draft-email-textarea.stories.js
@@ -0,0 +1,11 @@
+import examplePrototype from './example/example.twig';
+
+export default {
+  title: 'Prototypes/Draft Email Textarea',
+  parameters: {
+    docs: { page: null },
+    layout: 'fullscreen',
+  },
+};
+
+export const Example = () => examplePrototype({});

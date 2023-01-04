193eae962a1e7eb226ef902240113f0d49aa85bc
Gerardo Rodriguez <mr.gerardo.rodriguez@gmail.com>
2023-01-04 06:01:44 -0800

    Stable version

--- src/prototypes/draft-email-textarea/js/draft-email-textarea.js
+++ src/prototypes/draft-email-textarea/js/draft-email-textarea.js
@@ -1,24 +1,62 @@
-export const runPrototypeJavaScript = () => {
-  const copyToClipboard = (text) => {
-    if (navigator && navigator.clipboard && navigator.clipboard.writeText) {
-      return navigator.clipboard.writeText(text);
-    }
-    return Promise.reject('Clipboard API not available');
-  };
+/**
+ * The contents of this function will be inlined in a Gutenberg Custom HTML block
+ */
+export const runProposedInlineJS = () => {
+  // The elements all will be a part of the same Gutenberg Custom HTML block.
+  // I chose to use IDs since the HTML and JS are scoped to the same Gutenberg
+  // block (the JS functionaly won't be shared elsewhere)
+  const copyBtn = document.getElementById('copy-btn');
+  const copiedBtn = document.getElementById('copied-btn');
+  const draftEl = document.getElementById('email-draft');
+  const statusEl = document.getElementById('status-output');
+
+  // All the things required to run this feature
+  const requirements = [
+    copyBtn,
+    copiedBtn,
+    draftEl,
+    navigator,
+    navigator.clipboard,
+    navigator.clipboard.writeText,
+  ];
+  // Handle use case when all requirements are not met
+  if (!requirements.every((requirement) => requirement)) {
+    // Hide the "copy" btn since it will not be functional
+    if (copyBtn) copyBtn.hidden = true;
+    // Stop running this script
+    return;
+  }
 
   const onCopyClick = () => {
-    const draftEl = document.getElementById('email-draft');
+    // Add to clipboard
+    navigator.clipboard.writeText(draftEl.value);
+
+    // Swap visibility of "copy" and "copied" btns
+    copyBtn.hidden = true;
+    copiedBtn.hidden = false;
+    // Update the result of the action for more inclusive UX
+    statusEl.textContent = 'Text copied';
 
     // Select the text on click for a better UX
     draftEl.select();
     draftEl.setSelectionRange(0, 99999); // For mobile devices
 
-    copyToClipboard(draftEl.value);
+    // Reset after a brief delay
+    setTimeout(() => {
+      copyBtn.hidden = false;
+      copiedBtn.hidden = true;
+      statusEl.textContent = '';
+    }, 2000);
   };
 
-  document.getElementById('copy-btn').addEventListener('click', onCopyClick);
+  copyBtn.addEventListener('click', onCopyClick);
 };
 
+/**
+ * For the purposes of this prototype demo, this was the only way I could figure
+ * out how to include a string of text with spaces between each line of text.
+ * When I tried this in the MDX file, it would not work.
+ */
 export const forDemoPurposesOnlyText = `Hi there,
 
 My name is [Firstname Lastname], I’m a [role] at [organization].

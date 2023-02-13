e00b0be238232fca8c65faf400a468d9aac139a6
Tyler Sticka <tyler@cloudfour.com>
2023-02-13 15:02:48 -0800

    Improve highlight.php syntax highlighting

---
+++ .changeset/modern-flowers-remember.md
@@ -0,0 +1,5 @@
+---
+'@cloudfour/patterns': patch
+---
+
+Improves quality of syntax highlighting for JavaScript (and similar languages) using highlight.php 9.x
--- src/vendor/highlight/demo/samples/js.js
+++ src/vendor/highlight/demo/samples/js.js
@@ -2,11 +2,20 @@
 if (this !== 'that' || truth == false) {
   new RegExp(/ab+c/, 'i');
   boyHowdy(5, 'something');
+  console.log('hello world');
   return (2 * 4) / 3;
 }
 
-class Bread {
+class Bread extends Dough {
   constructor(slices = 12) {
+    if (slices > 24) {
+      throw new Error('Too much bread');
+    }
+
     this.slices = `There are ${slices} slices`;
   }
 }
+
+const sum = (...args) => {
+  return args.reduce((a, b) => a + b, 0);
+};

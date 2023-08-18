1df437059d1889c5131fdb2b5b5302137bebb219
Tyler Sticka <tyler@cloudfour.com>
2023-08-18 09:43:48 -0700

    R2 basic new footer layout

--- src/prototypes/new-footer/new-footer.stories.js
+++ src/prototypes/new-footer/new-footer.stories.js
@@ -1,5 +1,7 @@
 import r1Prototype from './r1.twig';
+import r2Prototype from './r2.twig';
 import './r1.scss';
+import './r2.scss';
 
 export default {
   title: 'Prototypes/New Footer',
@@ -10,3 +12,78 @@ export default {
 };
 
 export const r1 = () => r1Prototype({});
+
+export const r2 = () =>
+  r2Prototype({
+    organization: {
+      name: 'Cloud Four, Inc.',
+      url: 'https://cloudfour.com/',
+      address1: '510 SW 3rd Ave, Suite 420',
+      address2: 'Portland, OR 97204 USA',
+      phone: '+1 (503) 290-1090',
+      email: 'info@cloudfour.com',
+    },
+    nav: [
+      {
+        title: 'What We Do',
+      },
+      {
+        title: 'Our Approach',
+      },
+      {
+        title: 'Our Work',
+      },
+      {
+        title: 'Articles',
+      },
+      {
+        title: 'Speaking',
+      },
+      {
+        title: 'Team',
+      },
+      {
+        title: 'Hire Us',
+      },
+    ],
+    topics: [
+      {
+        title: 'Responsive Web Design',
+      },
+      {
+        title: 'Ecommerce',
+      },
+      {
+        title: 'Performance',
+      },
+      {
+        title: 'Images',
+      },
+      {
+        title: 'Accessibility',
+      },
+      {
+        title: 'Cloud Four Spotlight',
+      },
+      {
+        title: 'CSS',
+      },
+    ],
+    social: [
+      {
+        icon: 'brands/linkedin',
+      },
+      {
+        icon: 'brands/mastodon',
+      },
+      {
+        icon: 'brands/youtube',
+      },
+      {
+        icon: 'brands/instagram',
+      },
+      {
+        icon: 'feed',
+      },
+    ],
+  });
